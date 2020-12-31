3567caa13459392ba153ee2c3a3be33b47b30ad0
Tobi-273 <tobiasuhlig@gmx.de>
2020-12-31 10:50:10 +0100

    pop 8, imported numpy

--- batch generator.py
+++ batch generator.py
@@ -1,4 +1,7 @@
 from sklearn.utils import shuffle
+import numpy as np
+
+
 def generator(samples, batch_size=32):
 
     num_samples = len(samples)
@@ -6,7 +9,7 @@ def generator(samples, batch_size=32):
     while 1:
         shuffle(samples)
 
-        for div_point in range (0, num_samples, batch_size):
+        for div_point in range(0, num_samples, batch_size):
             batch_samples = samples[div_point:div_point+batch_size]
 
             images = []
@@ -15,10 +18,10 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 # here the extraction of the images and labels needs to go
 
-                images.append() #insert used variable for single images
-                labels.append() #insert used variable for single labels
+                images.append()  # insert used variable for single images
+                labels.append()  # insert used variable for single labels
 
-            X_train = np.array(images)
-            Y_train = np.array(labels)
+            x_train = np.array(images)
+            y_train = np.array(labels)
 
-            yield shuffle(X_train, Y_train)
+            yield shuffle(x_train, y_train)
